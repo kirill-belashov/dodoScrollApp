d0045afa7f66db3cc97ef044692a97e67f1a54fc
Kirill Belashov <udenjordisk@gmail.com>
2023-10-22 03:04:22 +0600

    Initial Commit

---
+++ DodoScrollApp/DodoScrollAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DodoScrollAppApp.swift
+//  DodoScrollApp
+//
+//  Created by Kirill Belashov on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DodoScrollAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
